753c91ec5227f7c1750dbe7f4caeeaad74bb015d
Thomas Frank <thomas@franks-im-web.de>
2025-06-14 08:15:03 +0200

    Add sender as bcc

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -1,6 +1,6 @@
 class ParticipantMailer < ApplicationMailer
   def registration_confirmation(participant)
     @participant = participant
-    mail(to: participant.email, subject: (t "mail.registation_confirmation.subject", tournament: @participant.Tournament.name))
+    mail(to: participant.email, bcc: ENV.fetch("ARCHER_DEFAULT_MAIL_ADDRESS", "from@example.com"), subject: (t "mail.registation_confirmation.subject", tournament: @participant.Tournament.name))
   end
 end
