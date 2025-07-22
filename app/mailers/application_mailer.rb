d69c260885cf698b5784ce622ca69bdfc45e3f5b
Thomas Frank <thomas@franks-im-web.de>
2025-07-22 21:50:03 +0200

    Add X-Mailer header to mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: (ENV.fetch("ARCHER_DEFAULT_MAIL_ADDRESS", "from@example.com"))
+  default from: (ENV.fetch("ARCHER_DEFAULT_MAIL_ADDRESS", "from@example.com")), "X-Mailer": "Archer"
   layout "mailer"
 end
