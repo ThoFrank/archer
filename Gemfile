1765ff7554e6662360efacb2e0747a5a56d23511
Thomas Frank <thomas@franks-im-web.de>
2025-10-10 22:52:01 +0200

    Add and configure omniauth_openid_connect

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "image_processing", "~> 1.2"
 # external authentication
 gem "omniauth"
 gem "omniauth-rails_csrf_protection"
+gem "omniauth_openid_connect"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,20 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
+  if ENV["ARCHER_OIDC_NAME"]
+    provider :openid_connect, {
+      name: ENV.fetch("ARCHER_OIDC_NAME"),
+      scope: [ :openid, :email ],
+      discovery: true,
+      response_type: :code,
+      uid_field: "email",
+      client_options: {
+        port: 443,
+        scheme: "https",
+        host: ENV.fetch("ARCHER_OIDC_HOST"),
+        identifier: ENV.fetch("ARCHER_OIDC_CLIENT_ID"),
+        secret: ENV.fetch("ARCHER_OIDC_SECRET_KEY"),
+        redirect_uri: ENV.fetch("ARCHER_OIDC_CALLBACK_URI")
+      }
+    }
+  end
 end
